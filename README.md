2cad8d411a09810c3108bd8015617d8bfd363e8c
Tennana <tennana@tennana.net>
2022-12-02 20:06:45 +0900

    fix: ビルド調整

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 定期更新型ゲーム、[影別のウタゲ](http://tyaunen.moo.jp/shadow/view/index.php)さんのVIVEタグを気が向いたままにWeb Componentsにしました。
 
 ## 試す
+### 単体ファイル生成
+```
+npm run build -- --mode components
+```
+
+distフォルダのjsを読み込んだ後、`<vive-shout text="イルカが攻めてきたぞ!"/>` などタグを記述してください。
+
+### Web Demo
+https://tennana.github.io/vive-tag-components/
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,32 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte({
-    build:{
-      lib:{
-        entry: './src/main.ts',
-        name: 'vive-tag-components',
-      }
-    },
-    compilerOptions: {
-      customElement: true,
-    },
-  })]
-})
+
+export default defineConfig(({command, mode, ssrBuild}) => {
+    if (command === 'serve' || mode !== 'components') {
+        return {
+            base: '/vive-tag-components/',
+            plugins: [svelte({
+                compilerOptions: {
+                    customElement: true,
+                },
+            })]
+        }
+    } else {
+        // command === 'build'
+        return {
+            build: {
+                lib: {
+                    entry: './src/main.ts',
+                    name: 'vive-tag-components',
+                }
+            },
+            plugins: [svelte({
+                compilerOptions: {
+                    customElement: true,
+                },
+            })]
+        }
+    }
+});
